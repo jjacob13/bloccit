647817b6316c597e870b21757e3e80f15ca2caa8
JD <jd.audiovisionary@gmail.com>
2015-12-17 00:05:19 -0400

    Added Answer spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) {Question.create!(title: "New Question Title", body: "New Question Body", resolved: false)}
+  let(:answer) {Answer.create!(body: "New answer body", question: question)}
+
+  describe "attributes" do
+    it "should respond to body" do
+      expect(answer).to respond_to(:body)
+    end
+  end
 end
