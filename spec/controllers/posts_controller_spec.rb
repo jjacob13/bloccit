41a2560b8e70ddab70430e8dc1b4d7e7a182fb76
JD <jd.audiovisionary@gmail.com>
2016-01-03 15:00:34 -0700

    More CRUD

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -61,11 +61,50 @@ RSpec.describe PostsController, type: :controller do
      end
    end
 
- #  describe "GET edit" do
- #    it "returns http success" do
- #      get :edit
- #      expect(response).to have_http_status(:success)
- #    end
- #  end
+   describe "GET edit" do
+     it "returns http success" do
+       get :edit, {id: my_post.id}
+       expect(response).to have_http_status(:success)
+    end
+    it "renders the #edit view" do
+      get :edit, {id: my_post.id}
+      expect(response).to render_template :edit
+    end
+    it "assigns post to be updated to @post" do
+      get :edit, {id: my_post.id}
+      post_instance = assigns(:post)
+      expect(post_instance.id).to eq my_post.id
+      expect(post_instance.title).to eq my_post.title
+      expect(post_instance.body).to eq my_post.body
+    end
+  end
 
+  describe "PUT update" do
+    it "updates post with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+      put :update, id: my_post.id, post: {title: new_title, body: new_body}
+      updated_post = assigns(:post)
+      expect(updated_post.id).to eq my_post.id
+      expect(updated_post.title).to eq new_title
+      expect(updated_post.body).to eq new_body
+    end
+    it "redirects to the updated post" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+      put :update, id: my_post.id, post: {title: new_title, body: new_body}
+      expect(response).to redirect_to my_post
+    end
+  end
+
+  def destroy
+     @post = Post.find(params[:id])
+     if @post.destroy
+       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+       redirect_to posts_path
+     else
+       flash.now[:alert] = "There was an error deleting the post."
+       render :show
+     end
+   end
 end
